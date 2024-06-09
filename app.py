5eef7bb4101adc6b3bc243f66e8dc80f40273281
yungshun317 <seraph317@gmail.com>
2024-06-10 03:23:02 +0800

    [FEAT] Display videos using the CSS grid layout

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import cv2
 
 app = Flask(__name__)
 
-cam = cv2.VideoCapture("rtsp://localhost:8554/stream")
-
-def gen_frames():
+def generate_frames():
     """Generate Frame by Frame from Camera"""
+    cam = cv2.VideoCapture("rtsp://localhost:8554/stream")
+
     while True:
         # Capture frame by frame
         ret, frame = cam.read()  # read the camera frame
         if not ret:
             break
         else:
+            # Resize
+            frame = cv2.resize(frame, (720, 720))
+
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
@@ -21,7 +24,7 @@ def gen_frames():
 @app.route('/video_feed')
 def video_feed():
     """Video Streaming Route. Put this in the src attribute of an img tag."""
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/")
 def index():
